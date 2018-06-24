0019892f3adf1c7d6e1b819211a47ef6ce751fe7
Marcin Kordus <marcin.kordus8@gmail.com>
2018-06-24 22:31:57 +0200

    initial commit

---
+++ src/main/java/com/test/reactivemongo/ReactivemongoApplication.java
@@ -0,0 +1,12 @@
+package com.test.reactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivemongoApplication.class, args);
+	}
+}
