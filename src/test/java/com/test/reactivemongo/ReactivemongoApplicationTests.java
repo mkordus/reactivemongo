e8d75c390ec158c9cbd744595c372834dedec426
Marcin Kordus <marcin.kordus8@gmail.com>
2018-06-25 08:12:10 +0200

    run tests mongo instance with TestContainers

--- src/test/java/com/test/reactivemongo/ReactivemongoApplicationTests.java
+++ src/test/java/com/test/reactivemongo/ReactivemongoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.test.reactivemongo;
 
+import com.mongodb.MongoClient;
 import com.test.reactivemongo.util.TestContainersExtension;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -11,7 +13,6 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.GenericContainer;
 
 @ExtendWith(TestContainersExtension.class)
@@ -21,20 +22,29 @@ import org.testcontainers.containers.GenericContainer;
 @ContextConfiguration(initializers = ReactivemongoApplicationTests.Initializer.class)
 public class ReactivemongoApplicationTests {
 
-	public static GenericContainer mongo = new GenericContainer("mongo:3.4.0").withExposedPorts(27017);
+    private static final String TEST_DB_NAME = "test";
+    public static GenericContainer mongo = new GenericContainer("mongo:3.4.0").withExposedPorts(27017);
 
-	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-		@Override
-		public void initialize(ConfigurableApplicationContext applicationContext) {
-			TestPropertyValues values = TestPropertyValues.of(
-					"spring.mongo.host=" + mongo.getContainerIpAddress(),
-					"spring.mongo.port=" + mongo.getMappedPort(27017)
-			);
-			values.applyTo(applicationContext);
-		}
-	}
+    @Autowired
+    private MongoClient mongoClient;
 
-	@Test
-	public void contextLoads() {
-	}
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues values = TestPropertyValues.of(
+                    "spring.data.mongodb.host=" + mongo.getContainerIpAddress(),
+                    "spring.data.mongodb.port=" + mongo.getMappedPort(27017)
+            );
+            values.applyTo(applicationContext);
+        }
+    }
+
+    @AfterEach
+    void setUp() {
+        mongoClient.getDatabase(TEST_DB_NAME).drop();
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
